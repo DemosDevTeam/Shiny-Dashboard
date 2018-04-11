f9ab3873704e2bca66af0add8c4f7d6e470d95fd
Alex Kan <lexokan@live.unc.edu>
2018-04-10 20:37:08 -0400

    Initial Commit

---
+++ global.R
@@ -0,0 +1,49 @@
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+result <- fromJSON(txt = "data/test2.json")
+
+n <- length(result$Users)
+df <- setNames(data.frame(matrix(ncol = 14, nrow = n)), c("gender",
+                                                         "race",
+                                                         "income",
+                                                         "age",
+                                                         "occupation", 
+                                                         "education", 
+                                                         "children",
+                                                         "marital"))
+
+df$age <- as.integer(df$age)
+
+    for (row in 1:length(result$Users)) {
+        user <- result$Users[[row]]
+        
+        if (is.null(user[["Reactions"]]) == F) {
+            user["Reactions"] <- NULL
+        }
+        
+        if (is.null(user[["engagement"]]) == F) {
+            user["engagement"] <- NULL
+        }
+        
+        if (is.null(user[["interests"]]) == F) {
+            user["interests"] <- NULL
+        }
+        
+        user <- as.tibble(user)
+        
+        for (col in colnames(user)) {
+            df[row, col] <- user[[col]]
+        }
+    }
+
+# remove NA columns
+df <- df[, colSums(is.na(df))<nrow(df)]
+df <- df %>% 
+    select(-(df$`update preferences`))
+    
+
+df <- df %>% 
+    drop_na()
+#df$children <- as.factor(df$children)
---
+++ ui.R
@@ -0,0 +1,46 @@
+#library(shiny)
+library(shinydashboard)
+
+shinyUI(fluidPage(
+    dashboardPage(skin = "red",
+    dashboardHeader(title = "Demos Dashboard"),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("About", tabName = "aboutTab", icon = icon("book")),
+            menuItem("Gender", tabName = "genderTab", icon = icon("transgender-alt"),
+                     radioButtons(inputId = "genderInput",
+                                  label = "Select a Gender",
+                                  c("All", "Female", "Male", "Non-binary", "Other", "Prefer not to respond"))),
+            menuItem("Race", tabName = "raceTab", icon = icon("users"),
+                     radioButtons(inputId = "raceInput",
+                                  label = "Select a Race",
+                                  c("All", "Black / African American", "Caucasian", "Asian / Pacific Islander", "Hispanic / Latinx", "Prefer not to respond"))),
+            menuItem("Income", tabName = "incomeTab", icon = icon("credit-card"),
+                     radioButtons(inputId = "incomeInput",
+                                  label = "Select an Income Range",
+                                  c("All", "0-20,000", "20,000-40,000", "40,000-60,000", "60,000-80,000", "80,000-100,000", "100,000+"))),
+            menuItem("Age", tabName = "ageTab", icon = icon("cog"),
+                     radioButtons(inputId = "ageInput",
+                                  label = "Select Age Range",
+                                  c("All","0-18", "18-21", "21-25", "25-30", "30-40", "40+"))),
+            menuItem("Occupation", tabName = "occupationTab", icon = icon("truck"),
+                     radioButtons(inputId = "occupationInput",
+                                  label = "Select Employment Area",
+                                  c("All", "Unemployed", "Employed, part-time", "Employed, full-time", "Employed, with multiple jobs"))),
+            menuItem("Education", tabName = "educationTab", icon = icon("pencil"),
+                     radioButtons(inputId = "educationInput",
+                                  label = "Select Education Level",
+                                  c("All", "No degree", "High school diploma", "Associate's degree", "Bachelor's degree", "Master's degree", "PhD"))),
+            menuItem("Children", tabName = "childrenTab", icon = icon("child"),
+                     radioButtons(inputId = "childrenInput",
+                                  label = "Select a Number of Children",
+                                  c("All", "None", "1-2", "3-5", "6+"))),
+            menuItem("Marital", tabName = "maritalTab", icon = icon("heart"),
+                     radioButtons(inputId = "maritalInput",
+                                  label = "Select Marital Status",
+                                  c("All", "Single, never married", "Single, divorced", "In a relationship", "Married", "Widowed")))
+        )),
+    dashboardBody("This is the body!"),
+    title = "Demos"
+)))
+
